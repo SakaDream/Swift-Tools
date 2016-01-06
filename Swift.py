23ea36e8af9ab05b84bc7ef87489ba865da057dc
SakaDream <bahaimt@gmail.com>
2016-01-06 17:15:41 +0700

    Update 01/06/2016 - 17:14:16
    Add shebang
    Can run for all users

--- Swift.py
+++ Swift.py
@@ -1,37 +1,37 @@
-from os import system
-import sys
-from os.path import exists
-from os.path import isdir
+#!/usr/bin/python
+import os, sys, os.path
 
 def help():
 	print("To create a Swift package, please command: python swift.py <your_project_name>")
 
 def check_workspace():
-	if isdir('/home/mint/swift/workspace') == True:
+	if os.path.isdir(os.path.expanduser("~") + '/swift/workspace') == True:
 		print("Checking workspace... Done!")
 	else:
 		print("Workspace is not exists. Making workspace...")
-		system("mkdir swift/workspace")
+		os.system("mkdir ~/swift/workspace")
 
 def run_command(n):
+	os.chdir(os.path.expanduser("~") + '/swift/workspace')
 	print("Making " + n + " project folder...")
-	system("mkdir swift/workspace/" + n)
+	os.system("mkdir " + n)
 	print("Making Package.swift...")
-	system("touch swift/workspace/" + n + "/Package.swift")
+	os.system("touch Package.swift")
 	print("Making Sources folder...")
-	system("mkdir swift/workspace/" + n + "/Sources")
+	os.system("mkdir " + n + "/Sources")
 	print("Making main.swift...")
-	system("touch swift/workspace/" + n + "/Sources/main.swift")
+	os.system("touch ~/swift/workspace/" + n + "/Sources/main.swift")
 	print("Openning main.swift...")
-	system("gedit swift/workspace/" + n + "/Sources/main.swift")
+	os.system("gedit ~/swift/workspace/" + n + "/Sources/main.swift")
 
 if len(sys.argv) == 1:
 	help()
 
 if len(sys.argv) == 2:
 	check_workspace()
-	if isdir('/home/mint/workspace/' + sys.argv[1]) == True:
-		print("This project is already exists. Please enter other project name.\n")
-		exit(0)
+	os.chdir(os.path.expanduser("~") + '/swift/workspace')
+	if os.path.isdir(sys.argv[1]) == True:
+		print("This project is already exists. Please enter other name.")
+		exit()
 	else:	
 		run_command(sys.argv[1])
